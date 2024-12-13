ef9ad325f7b84786906aa92334990a308190d12b
ehyde74 <tuukka.sales@iki.fi>
2024-12-14 00:16:47 +0200

    [Fix] subscribe decorator was not returning async wrapper for async methods
    This lead to visible issue that async callbacks were never scheduled/awaited for.

--- examples/testclient.py
+++ examples/testclient.py
@@ -13,26 +13,26 @@ mqtt = mqroute.MQTTClient(host="test.mosquitto.org")
 @mqtt.subscribe(topic="weather/+channel+/wflrealtime.txt", qos=mqroute.QOS.EXACTLY_ONCE, raw_payload=True)
 async def handle_weather1(topic: str, msg: mqroute.MQTTMessage, parameters: dict[str, Any]):
     payload_type = type(msg.message).__name__
-    logger.info("message: ")
-    logger.info(f"         topic: {topic}")
-    logger.info(f"         parameters: {parameters}")
-    logger.info(f"         {payload_type}: {msg.message}")
+    logger.info("message (handle_weather1): ")
+    logger.info(f"         1 topic: {topic}")
+    logger.info(f"         1 parameters: {parameters}")
+    logger.info(f"         1 {payload_type}: {msg.message}")
 
 @mqtt.subscribe(topic="weather/+channel+/wflwflexpj.json", qos=mqroute.QOS.EXACTLY_ONCE)
 async def handle_weather2(topic: str, msg: mqroute.MQTTMessage, parameters: dict[str, Any]):
     payload_type = type(msg.message).__name__
 
-    logger.info("message: ")
-    logger.info(f"         topic: {topic}")
-    logger.info(f"         parameters: {parameters}")
-    logger.info(f"         {payload_type}: {msg.message}")
+    logger.info("message:  (handle_weather2)")
+    logger.info(f"         2 topic: {topic}")
+    logger.info(f"         2 parameters: {parameters}")
+    logger.info(f"         2 {payload_type}: {msg.message}")
 
 @mqtt.subscribe(topic="weather/#", qos=mqroute.QOS.EXACTLY_ONCE, raw_payload=True)
 def handle_weather3(topic: str, msg: mqroute.MQTTMessage, _: dict[str, Any]):
     payload_type = type(msg.message).__name__
-    logger.info("message: ")
-    logger.info(f"         topic: {topic}")
-    logger.info(f"         {payload_type}: {msg.message}")
+    logger.info("message:  (handle_weather3)")
+    logger.info(f"         3 topic: {topic}")
+    logger.info(f"         3 {payload_type}: {msg.message}")
 
 
 
