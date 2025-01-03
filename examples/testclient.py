f5e97dbe8bd457ea4aa9e36a356ea99a97f81f8f
ehyde74 <tuukka.sales@iki.fi>
2025-01-03 21:24:24 +0200

    Add dynamic MQTT subscription method via functional interface
    Introduced `add_subscription` for dynamic topic subscriptions, allowing users to bind callbacks to MQTT topics at runtime using a functional approach. Also updated Python version requirement to 3.13 and incremented the project version to 0.2.2.

--- examples/testclient.py
+++ examples/testclient.py
@@ -56,15 +56,33 @@ def handle_weather3(topic: str, msg: mqroute.MQTTMessage, _: dict[str, Any]):
     logger.info("         3 topic:%s", topic)
     logger.info("         3 %s: %s", payload_type, msg.message)
 
-
 @mqtt.sigstop
 def sigstop_handler():
     """ Simple example of custom SIGSTOP handler callback."""
     logger.info("sigstop_handler called !!!!")
 
 
+async def handle_weather4(topic: str, msg: mqroute.MQTTMessage, _: dict[str, Any]):
+    """ Simple example method processing received MQTT messages that have matching topic
+     and are not processed by any other callback."""
+    payload_type = type(msg.message).__name__
+    logger.info("message:  (handle_weather4)")
+    logger.info("         4 topic:%s", topic)
+    logger.info("         4 %s: %s", payload_type, msg.message)
+
+
 async def main():
-    """ Exxample implementation of main running mqroute instance."""
+    """ Example implementation of main running mqroute instance."""
+
+    # register one of the handlers using functional interface (allows dynamic creation of
+    # subscriptions)
+    mqtt.add_subscription(handle_weather4,
+                          topic="weather/vt-dev/wflwflexpj.json",
+                          qos=mqroute.QOS.EXACTLY_ONCE,
+                          raw_payload=False,
+                          fallback=False)
+
+    # start mqroute client
     await mqtt.run()
 
     # Keep the client running
